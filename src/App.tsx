610119e7f8340b259cac46ceabb1ebbfc1b2e652
0sa0sa <osasou0125@gmail.com>
2024-12-24 23:24:15 +0900

    add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./stories/Header";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <Button>This is shadcn/ui button</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
