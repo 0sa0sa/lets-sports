bde8e6c205f026ad6b9f90b82c6ad1de31939cdd
0sa0sa <osasou0125@gmail.com>
2024-12-30 16:43:57 +0900

    remove Header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Header } from "@/stories/Header";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -9,7 +8,6 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div className="p-2">
-      <Header />
       <Button>This is shadcn/ui button</Button>
       <h3>Welcome Home!</h3>
     </div>
